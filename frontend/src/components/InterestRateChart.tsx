6ff9bcd931a259a08d89ab5406019ad3e4b3d899
Csonuzun <36796428+Csonuzun@users.noreply.github.com>
2025-01-14 03:30:50 +0300

    Update InterestRateChart.tsx

--- frontend/src/components/InterestRateChart.tsx
+++ frontend/src/components/InterestRateChart.tsx
@@ -4,6 +4,7 @@ import {ChartTooltip, ChartTooltipContent} from "@/components/ui/chart"
 import {Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle} from "./ui/card";
 import {TrendingUp} from "lucide-react";
 import {ChartLegend, ChartLegendContent} from "@/components/ui/chart"
+import React from "react";
 
 interface GraphData {
     labels: string[];
@@ -67,26 +68,39 @@ const InterestRateChart = ({data}: { data: GraphData }) => {
                         <ChartTooltip
                             content={
                                 <ChartTooltipContent
-                                    hideLabel
                                     formatter={(value, name) => (
-                                        <div className="flex min-w-[180px] items-center text-sm text-muted-foreground">
+                                        <>
+                                            <div
+                                                className="h-2.5 w-2.5 shrink-0 rounded-[2px] bg-[--color-bg]"
+                                                style={
+                                                    {
+                                                        "--color-bg": `var(--color-${name})`,
+                                                    } as React.CSSProperties
+                                                }
+                                            />
+                                            <div
+                                                className="flex min-w-[180px] items-center text-sm text-muted-foreground">
                                             <span className="font-semibold">
                                                 {chartConfig[name as keyof typeof chartConfig]?.label || name}
                                             </span>
-                                            <div
-                                                className="ml-auto flex items-baseline gap-0.5 font-mono font-medium tabular-nums text-foreground">
-                                                {value}
-                                                <span className="font-normal text-muted-foreground">%</span>
+                                                <div
+                                                    className="ml-auto flex items-baseline gap-0.5 font-mono font-medium tabular-nums text-foreground">
+                                                    {value}
+                                                    <span className="font-normal text-muted-foreground">%</span>
+                                                </div>
                                             </div>
-                                        </div>
+                                        </>
+
                                     )}
                                 />
                             }
                             cursor={false}
                         /> <ChartLegend content={<ChartLegendContent/>}/>
-                        <Line type="monotone" dataKey="getirFinans" stroke="rgb(75, 192, 192)" dot={false}
+                        <Line type="monotone" dataKey="getirFinans" stroke="rgb(75, 192, 192)"
+                              dot={false}
                               activeDot={true}/>
-                        <Line type="monotone" dataKey="enpara" stroke="rgb(255, 99, 132)" dot={false} activeDot={true}/>
+                        <Line type="monotone" dataKey="enpara" stroke="rgb(255, 99, 132)" dot={false}
+                              activeDot={true}/>
                     </LineChart>
                 </ChartContainer>
             </CardContent>
